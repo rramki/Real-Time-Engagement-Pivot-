8ad59d19586aabd486b33da95a679ca23f90a169
rramki <ramki21in@gmail.com>
2026-02-21 21:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,13 @@
 import cv2
 import streamlit as st
 import numpy as np
-
-
 from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")  # nano version
-
-results = model(frame)
-
-for r in results:
-    for box in r.boxes:
-        cls = int(box.cls[0])
-        if model.names[cls] == "cell phone":
-            st.error("Mobile Phone Detected!")
-
 import sounddevice as sd
 import numpy as np
-
-duration = 3
-audio = sd.rec(int(duration * 16000), samplerate=16000, channels=1)
-sd.wait()
-
-volume_norm = np.linalg.norm(audio) * 10
-
-if volume_norm < 1:
-    print("Silence detected")
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
-
-st.markdown("""
-<script>
-document.addEventListener("visibilitychange", function() {
-    if (document.hidden) {
-        alert("Tab switched!");
-    }
-});
-</script>
-""", unsafe_allow_html=True)
 
 st.title("Simple Virtual Proctor")
 
@@ -65,3 +33,33 @@ while run:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
 
     FRAME_WINDOW.image(frame, channels="BGR")
+
+
+model = YOLO("yolov8n.pt")  # nano version
+results = model(frame)
+
+for r in results:
+    for box in r.boxes:
+        cls = int(box.cls[0])
+        if model.names[cls] == "cell phone":
+            st.error("Mobile Phone Detected!")
+
+
+st.markdown("""
+<script>
+document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+        alert("Tab switched!");
+    }
+});
+</script>
+""", unsafe_allow_html=True)
+
+duration = 3
+audio = sd.rec(int(duration * 16000), samplerate=16000, channels=1)
+sd.wait()
+
+volume_norm = np.linalg.norm(audio) * 10
+
+if volume_norm < 1:
+    print("Silence detected")
