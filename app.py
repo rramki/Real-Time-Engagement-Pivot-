c8fa3fed1038ff69d13745e1967825c8a3d30dee
rramki <ramki21in@gmail.com>
2026-02-21 21:09:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,44 @@ import cv2
 import streamlit as st
 import numpy as np
 
+
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")  # nano version
+
+results = model(frame)
+
+for r in results:
+    for box in r.boxes:
+        cls = int(box.cls[0])
+        if model.names[cls] == "cell phone":
+            st.error("Mobile Phone Detected!")
+
+import sounddevice as sd
+import numpy as np
+
+duration = 3
+audio = sd.rec(int(duration * 16000), samplerate=16000, channels=1)
+sd.wait()
+
+volume_norm = np.linalg.norm(audio) * 10
+
+if volume_norm < 1:
+    print("Silence detected"
+
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
+
+st.markdown("""
+<script>
+document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+        alert("Tab switched!");
+    }
+});
+</script>
+""", unsafe_allow_html=True)
 
 st.title("Simple Virtual Proctor")
 
